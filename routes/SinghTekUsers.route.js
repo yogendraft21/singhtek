0942c07996cd8dfec2e69d53edfd662ad08e814c
yogendraft21 <yogendra311204@gmail.com>
2023-06-12 15:44:44 +0530

    merchant validation

--- routes/SinghTekUsers.route.js
+++ routes/SinghTekUsers.route.js
@@ -64,7 +64,7 @@ SinghTekRoute.post("/login", async (req, res) => {
 
           //on success generate token for user
           const token = jwt.sign({ userId: user._id }, process.env.TOKEN_KEY);
-          return res.status(200).json(token)
+          return res.status(200).json({ token: `${token}` });
         } else {
           return res.status(401).json("check email and password")
         }
